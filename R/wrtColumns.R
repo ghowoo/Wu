cc078157d3eed2083a2bade7ece3955fd71b7d6c
ghowoo <wu.gong@vumc.org>
2019-06-18 13:13:33 -0500

    my second commit

---
+++ R/wrtColumns.R
@@ -0,0 +1,20 @@
+#' A write columns Function
+#'
+#' This function allows you to write down your columns.
+#' @param 
+#' @keywords columns
+#' @export
+#' @examples
+#' wrtColumns
+
+
+wrtColumns <- function(obj,file){
+    require(Wu)
+    rtn <- data.frame(
+        name = colnames(obj)
+      , label = unlist(sapply(obj, label))
+      , class = unlist(lapply(sapply(data,class),function(x) x[length(x)]))
+    )
+    rownames(rtn) <- NULL
+    write.csv(rtn,file=file,row.names = FALSE)
+}
