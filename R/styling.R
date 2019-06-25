35fa79cf35795fc46543dd25271ed8f3f3b2a866
ghowoo <wu.gong@vumc.org>
2019-06-25 09:24:08 -0500

    my second commit

--- R/styling.R
+++ R/styling.R
@@ -7,18 +7,16 @@
 #' @examples
 #' styling()
 
-styling <- function(
-                full_width = FALSE
+styling <- function(...
+              ,  full_width = FALSE
               , bootstrap_options = c("striped", "hover", "condensed", "responsive")
               , position = "left"
               , fixed_thead = TRUE
-              , ...
                 ){
-    kableExtra::kable_styling(
-                    full_width = full_width
+    kableExtra::kable_styling(...
+                  , full_width = full_width
                   , bootstrap_options = bootstrap_options
                   , position = position
                   , fixed_thead = fixed_thead
-                  , ...
                 )
 }
