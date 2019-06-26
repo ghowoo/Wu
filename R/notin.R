4b2618276f8b0eeff07b9acd4fa3ce03f5d39e7e
ghowoo <wu.gong@vumc.org>
2019-06-26 11:37:11 -0500

    my second commit

---
+++ R/notin.R
@@ -0,0 +1,14 @@
+#' The NotIn Functions
+#'
+#' This function allows you to negate the in function.
+#' @param 
+#' @keywords negate notin
+#' @export
+#' @examples
+#'
+#' 
+
+
+`%notin%` <- Negate(`%in%`)
+
+
