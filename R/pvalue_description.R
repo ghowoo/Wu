887f48a36cfaa3ca0e5dc2e7906f9202e0b8f466
ghowoo <ghowoo@gmail.com>
2019-10-31 13:43:16 -0500

    my second commit

---
+++ R/pvalue_description.R
@@ -0,0 +1,20 @@
+#' A function describes how p values being calcualted
+#'
+#' This function describes how p values being calcualted
+#' @param 
+#' @keywords p-value
+#' @export
+#' @examples
+#' describe_pvalue()
+
+pvalue_description <- function(
+                               text = c(
+                                   "The default test for categorical variables is chi-squared test with Yates correction for continuity."
+                                 , "The default test for continuous variables is one-way ANOVA on means assuming equal variance. Kruskal-Wallis rank sum test has been applied for the variables where median and interquartile range (IQR) are show on the table.")
+                             , notation = "symbol"
+                               ) {
+    kableExtra::add_footnote(
+                    text
+                  , notation = notation
+                )
+}
