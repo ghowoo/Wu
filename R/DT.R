9ae646bc289a125ca1c1baf67efa591ad9605d6c
ghowoo <ghowoo@gmail.com>
2019-11-14 12:09:01 -0600

    my second commit

---
+++ R/DT.R
@@ -0,0 +1,22 @@
+#' A create a DT datatable
+#'
+#' Generate a DT datatable
+#' @param
+#' @keywords DT datatable
+#' @export
+#' @examples
+#'
+
+DT <- function(obj, ...){
+    DT::datatable(
+            data = obj
+          , filter = "top"
+          , rownames = FALSE
+          , extensions = "Buttons"
+          , options = list(
+                dom = "Bfrtip"
+              , buttons = I("colvis")
+            )
+          , ...
+        )
+}
