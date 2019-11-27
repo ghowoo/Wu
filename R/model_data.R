1366965440da20682566eacd045c2207711f1d4f
ghowoo <ghowoo@gmail.com>
2019-11-27 12:27:47 -0600

    my second commit

--- R/model_data.R
+++ R/model_data.R
@@ -3,15 +3,14 @@
 #' This function allows you to catch the data.frame from a model object
 #' @param
 #' @keywords model data.frame
-#' @export
 #' @examples
 #' model_data()
-
-
+#' @export
 model_data <- function(x, ...){
     UseMethod("model_data")
 }
 
+#' @export
 model_data.lmerMod <- function(obj){
     obj@frame
 }
