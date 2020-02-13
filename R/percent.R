68e0a1bc27091978960708c64e7e005aca80269b
ghowoo <ghowoo@gmail.com>
2020-02-13 15:05:01 -0600

    Feb 13th, 2020

--- R/percent.R
+++ R/percent.R
@@ -13,29 +13,15 @@ percent <- function(x, digits = 2, format = "f", ...) {
 
 #' @export
 fmtn <- function(x, decimals = 2){
-    format(round(x, decimals), nsmall = decimals)
+    format(round(x, decimals), nsmall = decimals, scientific = FALSE)
 }
 
 
 #' @export
-fmtp <- function(pvals, sig.limit = .0001, digits = 4, html = FALSE) {
-
-    roundr <- function(x, digits = 1) {
-        res <- sprintf(paste0('%.', digits, 'f'), x)
-        zzz <- paste0('0.', paste(rep('0', digits), collapse = ''))
-        res[res == paste0('-', zzz)] <- zzz
-        res
-    }
-
-    sapply(pvals, function(x, sig.limit) {
-        if (x < sig.limit)
-            if (html)
-                return(sprintf('&lt; %s', format(sig.limit)))
-            else
-                return(sprintf('< %s', format(sig.limit)))
-        if (x > .1)
-            return(roundr(x, digits = digits))
-        else
-            return(roundr(x, digits = digits))
-    }, sig.limit = sig.limit)
+fmtp <- function(x, sig = 4) {
+    sig_value <- 1 / (10 ^ sig)
+    if (x < sig_value)
+        return(paste0("< ", Wu::fmtn(sig_value, sig)))
+    else
+        return(fmtn(x, sig))
 }
