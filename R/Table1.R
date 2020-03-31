da7cda3036962c6c522d3afd31ba7ac478cce1b9
ghowoo <ghowoo@gmail.com>
2020-03-31 14:11:16 -0500

    March 31, 2020

--- R/Table1.R
+++ R/Table1.R
@@ -66,3 +66,74 @@ Table1 <- function(
     t <- cbind(t0[, 1:3], t1[, 3:ncol(t1)])
     return(t)
 }
+
+
+
+#' @export
+
+Table1n <- function(obj, Vars, FactorVars, Strata, ...){
+
+    table1_factor <- function(obj, var, Strata, Test = FALSE, ...){
+        Table1(
+            data = obj
+          , Vars = var
+          , FactorVars = var
+          , Strata = Strata
+          , Test = Test
+        )
+    }
+
+
+    table1_numeric1 <- function(obj, var, Strata, Test = FALSE, ...){
+        Table1(
+            data = obj
+          , Vars = var
+          , Strata = Strata
+          , Test = Test
+        )
+    }
+
+    table1_numeric2 <- function(obj, var, Strata, Test = FALSE, ...){
+        Table1(
+            data = obj
+          , Vars = var
+          , NonNormals = var
+          , Strata = Strata
+          , Test = Test
+        )
+    }
+
+    table1_numeric3 <- function(obj, var, Strata, Test = FALSE, ...){
+        Table1(
+            data = obj
+          , Vars = var
+          , NonNormals = var
+          , minMax = TRUE
+          , Strata = Strata
+          , Test = Test
+        )
+    }
+
+    table1_single <- function(obj, var, Strata, FactorVars, ...){
+        if (var %in% FactorVars) {
+            return(table1_factor(obj, var, Strata, ...))
+        } else {
+            return(rbind(
+                table1_numeric1(obj, var, Strata, ...)
+              , table1_numeric2(obj, var, Strata, ...)[2, ]
+              , table1_numeric3(obj, var, Strata, ...)[2, ]
+            ))
+        }
+    }
+    
+    for (i in 1:length(Vars)){
+        if(i == 1){
+            rtn <- table1_single(obj, Vars[i], Strata, FactorVars, ...)
+        } else {
+            rtn <- rbind(rtn
+                       , table1_single(obj, Vars[i], Strata, FactorVars, ...)
+                         )
+        }
+    }
+    return(rtn)
+}
