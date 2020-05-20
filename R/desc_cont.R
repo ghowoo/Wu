ae8a8f3eea235801c10b5ddbd6a09404e94806ae
ghowoo <ghowoo@gmail.com>
2020-05-20 15:58:12 -0500

    May 20th, 2020

--- R/desc_cont.R
+++ R/desc_cont.R
@@ -1,17 +1,17 @@
 #' A desc_cont Function
 #'
 #' Describe a continuous variable.
-#' @param 
+#' @param
 #' @keywords describe continuous variable
-#' @export
 #' @examples
 #' x <- c(rnorm(100), rep(NA, 20))
 #' desc_cont(x)
-
+#' @export
 desc_cont <- function(x, digits_p = 1, digits_c = 3) {
     n <- length(x)
     n_missing <- sum(is.na(x))
     n_nonmissing <- n - n_missing
+    n_unique <- length(unique(x))
     c(
         paste0("N: ", as.character(n))
       , paste0(
@@ -28,6 +28,7 @@ desc_cont <- function(x, digits_p = 1, digits_c = 3) {
           , Wu::percent(n_missing / n, digits_p)
           , ")"
         )
+      , paste0("Unique: ", as.character(n_unique), " (", Wu::percent(n_unique/n, digits_p), ")")
       , paste0(
             "Mean: "
           , format(mean(x, na.rm = TRUE), digits = digits_c)
@@ -65,4 +66,31 @@ desc_cont <- function(x, digits_p = 1, digits_c = 3) {
           , format(max(x, na.rm = TRUE) - min(x, na.rm = TRUE), digits = digits_c)
         )
     )
+}
+
+#' @export
+desc_cat <- function(x, digits_p=1, digits_c=3){
+    n <- length(x)
+    n_missing <- sum(is.na(x))
+    n_nonmissing <- n - n_missing
+    n_unique <- length(unique(x))
+    t <- table(x)
+    t <- t[order(-t)][1:min(7, length(t))]
+    attr(t, "dimnames")[[1]] <- substring(attr(t, "dimnames")[[1]], first=1, last=20)
+    cats <- unlist(lapply(1:length(t), function(x){
+        paste0(dimnames(t)[[1]][x]
+             , ": "
+             , as.character(t[x])
+             , " ("
+             , Wu::percent( t[x] / n, digits_p)
+             , ")", collapse = "")
+    }))
+    c(paste0("N: ", as.character(n))
+    , paste0("Non-Missing: ", as.character(n_nonmissing), " (", Wu::percent(n_nonmissing/n, digits_p), ")")
+    , paste0("Missing: ", as.character(n_missing), " (", Wu::percent(n_missing/n, digits_p), ")")
+    , paste0("Unique: ", as.character(n_unique), " (", Wu::percent(n_unique/n, digits_p), ")")
+    , paste0("Min: ", min(x, na.rm = TRUE))
+    , paste0("Max: ", max(x, na.rm = TRUE))
+    , cats
+      )
 }
