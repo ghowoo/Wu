316581641723cd9f05173e90edfb6989ed451f16
ghowoo <ghowoo@gmail.com>
2020-10-28 10:44:02 -0500

    Modify folowchart.R, make size and width fit and remove hierarchical

--- R/flowchart.R
+++ R/flowchart.R
@@ -1,33 +1,22 @@
 #' A flowchart Function
 #'
 #' This function allows you to create a flowchart
-#' @param 
+#' @param
 #' @keywords flowchart
 #' @export
 
 
-
-
-
-flowchart <- function(data, vars, top = "Total", font_size = 30, icon_size = 80, edge_width = 10, plot_height = "800px", plot_width = "100%"){
-require(visNetwork)
+flowchart <- function(data, vars, top = "Total", font_size = 15, icon_size = 40, edge_width = 2, plot_height = "800px", plot_width = "100%"){
+    require(visNetwork)
     nodes <-  get_nodes(
         data
       , vars
       , top)
-
     nodes$font.size <- font_size
-    nodes$size <- nodes$size * icon_size
-    
+    nodes$size <- sqrt(nodes$size) * icon_size
     edges <- get_edges(nodes)
-    edges$width <- edges$width * edge_width
-
+    edges$width <- sqrt(edges$width) * edge_width
     visNetwork::visNetwork(nodes, edges, height = plot_height, width = plot_width) %>%
         visOptions(highlightNearest = TRUE) %>%
-        visLayout(randomSeed = 123) %>%
-        visLayout(hierarchical = TRUE) %>%
-        visHierarchicalLayout(direction = "LR", levelSeparation = 500) %>%
-        addFontAwesome() %>%
-        visOptions(collapse = TRUE)
+        addFontAwesome()
 }
-
