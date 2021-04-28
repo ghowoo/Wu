3b035005688a3c555072d2adf9490b39f22aeccf
ghowoo <ghowoo@gmail.com>
2021-04-28 11:41:06 -0500

    my second commit

--- R/prt.R
+++ R/prt.R
@@ -5,7 +5,15 @@
 #' @keywords print a table to html
 #' @export
 
+
+
 prt <- function(dt, ...){
+  knitr::kable(dt, align = "lrrrrrrrr", ...) %>%
+    Wu::styling()
+}
+
+#' @export
+prt1 <- function(dt, ...){
   dt <- as.data.table(dt)
   dt <- dt[, what_locf := what[nafill(replace(.I, what %in% c(""), NA), type="locf")]]
   group_index <- auto_index(dt$what_locf)
